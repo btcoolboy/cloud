655accfd62a3eb7a1ec4cf0097a685f6cc65682f
Guofei Tan <61384451+btcoolboy@users.noreply.github.com>
2022-10-29 23:24:38 +0800

    Update bootstrap.sh

--- cephadm/bootstrap.sh
+++ cephadm/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install cephadm ceph-common -y
 sudo cephadm --docker bootstrap \
-        --image 10.200.101.20:8084/ceph/ceph:v15
+        --image 10.200.101.20:8084/ceph/ceph:v15 \
 	--apply-spec ./cluster.yaml \
 	--ssh-user ubuntu \
 	--ssh-private-key /home/ubuntu/.ssh/id_rsa \
