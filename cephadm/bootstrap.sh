1a246912ad68be4d90e00a0d9a5823ca52311a32
Guofei Tan <61384451+btcoolboy@users.noreply.github.com>
2023-02-17 22:40:16 +0800

    Update bootstrap.sh

--- cephadm/bootstrap.sh
+++ cephadm/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "deb http://vm-data-20.cloud.lab:8081/repository/ceph/debian-quincy/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/ceph.list
 sudo apt update && sudo apt install cephadm ceph-common -y
 sudo cephadm --docker --image 10.200.101.20:8084/ceph/ceph:v17 bootstrap \
 	--apply-spec ./cluster.yaml \
